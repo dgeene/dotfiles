08ef7edd41a3216aaed748320f3ac89a7f106a56
dgeene <david.geene@gmail.com>
2015-08-01 22:47:39 -0400

    begin work on bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # Script to prep my environment on a newly installed system
-# and will replace makesymlinks.sh
+# Designed for debian/ubuntu systems
 
 
 # TODO
@@ -15,3 +16,43 @@
 #
 # ideas for bashrc
 # 1. use an amber color for non-local bash sessions
+
+
+#TODO put sudo check here
+# If we're on a server and we don't have sudo privileges
+# then do a best effort bootstrapping
+
+cat << SetupMessage
+
+
+This script is meant to be run on a fresh install of a debian/ubuntu system.
+--Apt Installs--
+tmux
+vim
+--Git Repos--
+tmuxifier
+
+
+SetupMessage
+
+
+dir=~/dotfiles
+olddir=~/old_dotfiles
+symlinks="vim vimrc tmux.conf"
+
+main () {
+
+    echo "installing our favorite packages..."
+    sleep 1
+    #apt-get -y install vim tmux git
+
+    echo "Creating symlinks for $symlinks"
+    for link in $symlinks; do
+        echo "$link"
+    done
+
+
+}
+
+
+main
